6db3559aa90ae1d51c833c619944d11ca8ef9b7e
Produlx <dulsathlenovo12@gmail.com>
2025-05-25 22:10:17 +0530

    added last  rain

--- water meter.py
+++ water meter.py
@@ -1,4 +1,6 @@
-water_limit = int(input("Enter You had water count: "))
+water_limit = int(input("what count of water you want: "))
 water_last_limit = int(int("Enter your last god water qty: "))
+water_rain_week = int(input("Enter last week rain presentation: "))
 print("")
+
 
