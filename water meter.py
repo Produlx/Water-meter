6c253e5baef25395805a531487d3fced50b23fa3
Produlx <dulsathlenovo12@gmail.com>
2025-05-26 23:45:25 +0530

    added some space

--- water meter.py
+++ water meter.py
@@ -1,4 +1,5 @@
 water_limit = int(input("what count of water you want: "))
+print("--------------------------------------------------------------------------------------------------------")
 water_last_limit = int(int("Enter your last god water qty: "))
 water_rain_week = int(input("Enter last week rain presentation: "))
 print("")
@@ -7,4 +8,4 @@ if water_limit <= 50:
     print("ok")
 else:
     print("Your water limit is good,no need")
-    
+
